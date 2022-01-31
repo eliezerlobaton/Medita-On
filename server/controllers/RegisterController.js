d5a402d2a9c90e8e129d3bd1e2750b4283ca9623
Gustavo Mendes <g.mendes93@hotmail.com>
2022-01-30 23:51:58 -0300

    Merge branch 'dev' of https://github.com/eliezerlobaton/Medita-On into feature/CRUD-Usuarios

--- server/controllers/RegisterController.js
+++ server/controllers/RegisterController.js
@@ -9,20 +9,20 @@ const controller = {
     create: async (req, res, next) => {
         const { frist_name, birth, last_name, email, password } = req.body;
 
-        console.log(frist_name)
-
-        
-
-
         try {
             const newUser = await Users.create({
                 frist_name: frist_name,
                 birth, last_name, email, password });
 
-            res.status(200).json({
-                message: "Conta criada com sucesso!",
-                newUser
+            // res.status(200).json({
+            //     message: "Conta criada com sucesso!",
+            //     newUser
+            // });
+
+            res.render('login', {
+                message: 'Conta criada com sucesso!',
             });
+
         }catch(error){
             res.status(400).json({
                 message: "Erro ao criar conta.",
@@ -30,6 +30,8 @@ const controller = {
             });
         }
     },
+
+    
 }
 
 module.exports = controller;
