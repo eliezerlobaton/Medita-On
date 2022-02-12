be2bf496e00d512cf938d328ef1a2278d9f4b144
eliezer <eliezerlobaton39@gmail.com>
2022-02-11 21:23:46 -0300

    ajustando middleware de autenticação e admin

--- server/app.js
+++ server/app.js
@@ -1,38 +1,40 @@
 const createError = require("http-errors"),
   express = require("express"),
-  bodyParser = require('body-parser'),
   path = require("path"),
   cookieParser = require("cookie-parser"),
-  logger = require("morgan")
-
-const app = express();
-
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+  logger = require("morgan"),
+  session = require("express-session");
 
 // ARQUIVOS DE ROTAS - IMPORTAÇÃO
-const indexRouter = require('./routes/index')
-const adminRouter = require('./routes/admin')
-
-
+const indexRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
+  
 // ARQUIVO MIDDLEWARE - IMPORTAÇÃO
 // const adminMiddleware = require("./middlewares/admin");
-
+  
 // APP
+const app = express();
 
-// PASTA PÚBLICA PARA ARQUIVOS ESTÁTICOS (IMG, JS, CSS...)
-app.use(express.static(path.join(__dirname, "public")));
 // VIEW ENGINE - A PASTA DAS VIEWS E A SINTAXE (EJS)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use('/', indexRouter)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+app.use(session({ secret: 'QWhNdWxla2U=', cookie: { maxAge: 60000 } }))
+
+// PASTA PÚBLICA PARA ARQUIVOS ESTÁTICOS (IMG, JS, CSS...)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', indexRouter);
 
 
-// app.use(adminMiddleware)
+// app.use(adminMiddleware);
 
-app.use("/admin", adminRouter)
+app.use("/admin", adminRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
